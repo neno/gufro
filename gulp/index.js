0216a3d415c40611a6592f6d1b688b1c9ddb1ae1
Nenad Brcic <neno@neno.ch>
2015-05-22 15:31:35 +0200

    Fixing path to templates and components

--- gulp/index.js
+++ gulp/index.js
@@ -6,82 +6,37 @@ var gulp = require('gulp'),
     fs = require('fs'),
     path = require('path');
 
-//gulp.task('index-templates', function() {
-//    var templates = [],
-//        components = [],
-//        json = __dirname + '/../data/index.json';
-//
-//    $.util.log('Populate /data/index.json with template data');
-//
-//    return gulp
-//        .src(config.templates)
-//        .pipe($.plumber())
-//        .pipe($.data(function(file) {
-//            var content = fm(String(file.contents));
-//            var f = getFilepath(file, 'templates/');
-//
-//            content.attributes.fileName = f;
-//            templates.push(content.attributes);
-//
-//            fs.writeFileSync(json, JSON.stringify({
-//                templates: templates
-//            }));
-//        }));
-//});
-//
-//gulp.task('index-components', function() {
-//    var components = [],
-//        json = __dirname + '/../data/components.json';
-//
-//    $.util.log('Populate /data/components.json with components data');
-//
-//    return gulp
-//        .src(config.components)
-//        .pipe($.plumber())
-//        .pipe($.data(function(file) {
-//            var content = fm(String(file.contents));
-//            var f = getFilepath(file, 'components/');
-//
-//            content.attributes.fileName = f;
-//            components.push(content.attributes);
-//
-//            fs.writeFileSync(json, JSON.stringify({
-//                components: components
-//            }));
-//        }));
-//});
-
 gulp.task('index', function() {
     createIndex('templates');
     createIndex('components');
 });
 
-function createIndex(type) {
+function createIndex(context) {
     var data = [],
         jsonData = {},
-        json = __dirname + '/../data/' + type +'.json';
+        json = __dirname + '/../data/' + context +'.json';
 
-    $.util.log('Populate /data/' + type + '.json with components data');
+    $.util.log('Populate /data/' + context + '.json with components data');
 
     return gulp
-        .src(config[type])
+        .src(config[context])
         .pipe($.plumber())
         .pipe($.data(function(file) {
             var content = fm(String(file.contents));
-            var f = getFilepath(file, type + '/');
+            var f = getFilepath(file, context);
 
             content.attributes.fileName = f;
             data.push(content.attributes);
-
-            jsonData[type] = data;
-
+            jsonData[context] = data;
             fs.writeFileSync(json, JSON.stringify(jsonData));
         }));
 }
 
-function getFilepath(file, prefix) {
-    var f = file.path.split('/').pop();
-    f = prefix + f;
+function getFilepath(file, context) {
+    var urlParts = file.path.split('/'),
+        idx = urlParts.indexOf(context),
+        f = urlParts.slice(idx).join('/');
+
     f = f.replace('jade', 'html');
     return f;
 }
